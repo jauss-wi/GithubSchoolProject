ad6338bb91364a0ad1e9c0628af2d6e2dff78bc9
jauss-wi <jauss-wi@gmx.com>
2025-03-05 18:53:16 +0000

    Add 2.ts

---
+++ src/2.ts
@@ -0,0 +1,7 @@
+// Function to generate a random number between 1 and 10
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+// Test the function
+console.log(getRandomNumber());
