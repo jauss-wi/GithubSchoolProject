aad8f644762242d84aba3a0f8fd4313a5ebfabff
jauss-wi <jauss-wi@gmx.com>
2025-03-08 05:44:54 +0000

    Add 5.ts

---
+++ src/5.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-githubschoolproject',
+  templateUrl: './githubschoolproject.component.html',
+  styleUrls: ['./githubschoolproject.component.css']
+})
+export class GithubSchoolProject implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
